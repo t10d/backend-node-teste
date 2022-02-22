f7af2adc6c3516569f983974a21eabfd5cac1ec7
Joismar <joismar@outlook.com>
2022-02-22 19:24:02 -0300

    refactor: create new error

---
+++ src/presentation/errors/param-errors.ts
@@ -0,0 +1,13 @@
+export class MissingParamError extends Error {
+  constructor (paramName: string) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
+  }
+}
+
+export class InvalidParamError extends Error {
+  constructor (paramName: string) {
+    super(`Invalid param: ${paramName}`)
+    this.name = 'InvalidParamError'
+  }
+}
