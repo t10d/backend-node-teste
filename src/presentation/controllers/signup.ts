0ffcd6380ba22def8e4723568ef2b249dcb3b564
Joismar <joismar@outlook.com>
2022-02-22 18:46:33 -0300

    refactor: update SignupController for use http interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,13 @@
 import { MissingParamError } from "../errors/missing-param-error"
+import { badRequest } from "../helpers/http-helper"
+import { HttpRequest, HttpResponse } from "../interfaces/http"
 
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return new MissingParamError(field)
+        return badRequest(new MissingParamError(field))
       }
     }
     return {
