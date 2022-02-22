bf29192c3346687da79d45803194ddbe86f0867b
Joismar <joismar@outlook.com>
2022-02-22 20:14:10 -0300

    test: SignupController should returns 500 if server error

--- tests/presentation/controllers/signup.spec.ts
+++ tests/presentation/controllers/signup.spec.ts
@@ -1,5 +1,6 @@
 import { SignUpController } from "../../../src/presentation/controllers/signup"
 import { InvalidParamError, MissingParamError } from "../../../src/presentation/errors/param-errors"
+import { ServerError } from "../../../src/presentation/errors/server-error"
 import { EmailValidator } from "../../../src/presentation/interfaces/email-validator"
 
 interface SUTTypes {
@@ -97,5 +98,29 @@ describe('SignupController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should return 500 if email validator throw an error', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+
+    const httpRequest = {
+      body: {
+        name: 'name',
+        email: 'email@email.com',
+        password: 'abcde',
+        passwordConfirmation: 'abcde'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
