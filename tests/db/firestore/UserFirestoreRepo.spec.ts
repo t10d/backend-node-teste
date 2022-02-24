4aa4db2d340cd63d047287f69d669de48da5c200
Joismar <joismar@outlook.com>
2022-02-24 15:29:39 -0300

    test: ensure Firestore return correct useron success

---
+++ tests/db/firestore/UserFirestoreRepo.spec.ts
@@ -0,0 +1,37 @@
+import { AddUserRepository } from "../../../src/data/interfaces/addUserRepo"
+import { UserFirestoreRepo } from "../../../src/infra/db/firestore/userFirestoreRepo"
+import { db } from "../../../src/infra/db/firestore/helpers/firestoreHelper"
+interface SUTTypes {
+  sut: AddUserRepository
+}
+
+const makeSUT = (): SUTTypes => {
+  const sut = new UserFirestoreRepo()
+  return {
+    sut
+  }
+}
+
+describe('AddUser Repository', () => {
+  test('Should return an user on success', async () => {
+    const { sut } = makeSUT()
+
+    const user = await sut.add({
+      name: 'name',
+      email: 'email@email.com',
+      password: 'hashed_password'
+    })
+
+    expect(user).toBeTruthy()
+    expect(user.id).toBeTruthy()
+    expect(user.name).toBe('name')
+    expect(user.email).toBe('email@email.com')
+    expect(user.password).toBe('hashed_password')
+  })
+
+  test('Should throws error to SignupController then throws', async () => {
+    const { sut } = makeSUT()
+
+    // pass
+  })
+})
