ee23480d02a289702ac4ff998201b9108246218b
Joismar <joismar@outlook.com>
2022-02-24 20:49:05 -0300

    feat: add cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/bodyParser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
