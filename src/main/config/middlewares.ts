c3104fe3cddaba184e2ed8e2199f81fda81e6e3d
Joismar <joismar@outlook.com>
2022-02-24 20:54:01 -0300

    test: ensure return content type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/bodyParser'
+import { contentType } from '../middlewares/contentType'
 import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
