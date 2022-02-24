19f039855135aaefa3e95243f6a7758d78a142c5
Joismar <joismar@outlook.com>
2022-02-23 22:33:43 -0300

    feat: ensure bcrypt error to be catched by SignUpController

--- src/infra/security/bcriptAdapter.ts
+++ src/infra/security/bcriptAdapter.ts
@@ -5,9 +5,7 @@ export class BcriptAdapter implements Encrypter {
   constructor (private readonly saltRounds = 10) {}
 
   async encrypt (value: any): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const hashedValue = bcrypt.hash(value, this.saltRounds)
-      return resolve(hashedValue)
-    })
+    const hashedValue = await bcrypt.hash(value, this.saltRounds)
+    return hashedValue
   }
 }
