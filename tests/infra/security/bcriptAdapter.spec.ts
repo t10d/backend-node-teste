3e34c01b18288dc15cd62feb11496006931445a6
Joismar <joismar@outlook.com>
2022-02-23 21:55:41 -0300

    test: ensure bcrypt return a hash on success

--- tests/infra/security/bcriptAdapter.spec.ts
+++ tests/infra/security/bcriptAdapter.spec.ts
@@ -16,6 +16,12 @@ const makeSUT = (): SUTTypes => {
   }
 }
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hashedValue'))
+  }
+}))
+
 describe('Bcript Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const { sut, salt } = makeSUT()
@@ -23,5 +29,12 @@ describe('Bcript Adapter', () => {
     await sut.encrypt('any_value')
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const { sut } = makeSUT()
+    const hashedValue = await sut.encrypt('any_value')
+
+    expect(hashedValue).toBe('hashedValue')
   })
 })
