3c65de52e56b9d3f9f98d3a8a1cf360e41aafbac
Joismar <joismar@outlook.com>
2022-02-25 16:18:59 -0300

    feat: create logController decorator

--- src/main/factories/signUp.ts
+++ src/main/factories/signUp.ts
@@ -2,13 +2,16 @@ import { DbAddUser } from "../../data/useCases/addUser/dbAddUser";
 import { UserFirestoreRepo } from "../../infra/db/firestore/userFirestoreRepo";
 import { BcriptAdapter } from "../../infra/security/bcriptAdapter";
 import { SignUpController } from "../../presentation/controllers/signup/signUp";
+import { Controller } from "../../presentation/interfaces";
 import { EmailValidatorAdapter } from "../../utils/emailValidatorAdapter";
+import { LogControllerDecorator } from "../decorators/logControllerDecorator";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const encrypter = new BcriptAdapter(salt)
   const userFirestoreRepo = new UserFirestoreRepo()
   const dbAddUser = new DbAddUser(encrypter, userFirestoreRepo)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  return new SignUpController(emailValidatorAdapter, dbAddUser)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddUser)
+  return new LogControllerDecorator(signUpController)
 }
