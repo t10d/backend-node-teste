8a03a02cf51316faa406c5e6d776bef1ab438930
Joismar <joismar@outlook.com>
2022-02-27 18:03:06 -0300

    feat: add getByEmail and updateAccessToken to firestore repo

--- src/infra/db/firestore/userFirestoreRepo.ts
+++ src/infra/db/firestore/userFirestoreRepo.ts
@@ -1,9 +1,10 @@
 import { AddUserRepo } from "../../../data/interfaces/db/addUserRepo"
+import { GetUserByEmailRepo } from "../../../data/interfaces/db/getUserByEmailRepo"
 import { UserModel } from "../../../domain/models"
 import { AddUserModel } from "../../../domain/useCases"
 import { FirestoreHelper } from "../firestore/helpers/firestoreHelper"
 
-export class UserFirestoreRepo implements AddUserRepo {
+export class UserFirestoreRepo implements AddUserRepo, GetUserByEmailRepo {
   async add (userData: AddUserModel): Promise<UserModel> {
     const user = FirestoreHelper.getCollection('users').doc()
     const userObject = { id: user.id, ...userData }
@@ -12,4 +13,19 @@ export class UserFirestoreRepo implements AddUserRepo {
     
     return new Promise(resolve => resolve(userObject))
   }
+
+  async getByEmail(email: string): Promise<UserModel> {
+    const usersCol = FirestoreHelper.getCollection('users')
+    const usersSnapshot = await usersCol.where('email', '==', email).get()
+    if (!usersSnapshot.empty) {
+      const userDoc = usersSnapshot.docs[0].data()
+      return FirestoreHelper.map(userDoc)
+    }
+    return null
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const userDoc = FirestoreHelper.getCollection('users').doc(id)
+    await userDoc.update({ accessToken: token })
+  } 
 }
