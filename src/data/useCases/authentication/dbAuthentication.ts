5371d247c8ee11bc36201e1c4c2c4d550fa5a640
Joismar <joismar@outlook.com>
2022-02-27 14:34:48 -0300

    feat: add TokenGenerator to DbAuth

--- src/data/useCases/authentication/dbAuthentication.ts
+++ src/data/useCases/authentication/dbAuthentication.ts
@@ -1,19 +1,24 @@
 import { Authentication, AuthModel } from "../../../domain/useCases/authentication"
 import { LoadUserByEmailRepo } from "../../interfaces/loadUserByEmailRepo";
 import { HashComparer } from "../../interfaces/security/hashComparer";
+import { TokenGenerator } from "../../interfaces/security/tokenGenerator";
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly loadUserByEmailRepo: LoadUserByEmailRepo,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth(auth: AuthModel): Promise<string> {
     const user = await this.loadUserByEmailRepo.load(auth.email)
     if (user) {
-      await this.hashComparer.compare(auth.password, user.password)
+      const comparerResult = await this.hashComparer.compare(auth.password, user.password)
+      if (comparerResult) {
+        const accessToken = await this.tokenGenerator.generate(user.id)
+        return accessToken
+      }
     }
-
     return null
   }
 }
