0c235688244e7b1d99901f5fcd45509f35b5be01
Joismar <joismar@outlook.com>
2022-02-27 12:54:44 -0300

    feat: integration of Validation with signup controller

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,37 +1,21 @@
-import { InvalidParamError, MissingParamError } from "../../errors"
-import { badRequest, ok, serverError } from "../../helpers/http-helpers"
-import { EmailValidator, AddUser, Controller, HttpRequest, HttpResponse } from "./interfaces"
+import { badRequest, ok, serverError } from "../../helpers/httpHelpers"
+import { AddUser, Controller, HttpRequest, HttpResponse, Validation } from "./interfaces"
 
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
-  private readonly addUser: AddUser
-
-  constructor (emailValidator: EmailValidator, addUser: AddUser) {
-    this.emailValidator = emailValidator
-    this.addUser = addUser
-  }
+  constructor (
+    private readonly addUser: AddUser, 
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const error = this.validation.validate(httpRequest.body)
 
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+      if (error) {
+        return badRequest(error)
       }
 
-      const { name, email, password, passwordConfirmation } = httpRequest.body
-
-      if (password !== passwordConfirmation) {
-        return badRequest(new InvalidParamError('passwordConfirmation'))
-      }
-
-      const isValid = this.emailValidator.isValid(email)
-
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
+      const { name, email, password } = httpRequest.body
 
       const user = await this.addUser.add({
         name, email, password
