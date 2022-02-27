7b3625c57925df09b4f6efd335749066514c1cf0
Joismar <joismar@outlook.com>
2022-02-27 13:49:43 -0300

    test: ensure DbAuth call LoadUserByEmail with correct email

---
+++ tests/data/useCases/dbAuthetication.spec.ts
@@ -0,0 +1,65 @@
+import { Encrypter } from "../../../src/data/interfaces/encripter"
+import { AddUserRepo } from "../../../src/data/interfaces/addUserRepo"
+import { UserModel } from "../../../src/domain/models"
+import { LoadUserByEmailRepo } from "../../../src/data/interfaces/loadUserByEmailRepo"
+import { DbAuthentication } from "../../../src/data/useCases/authentication/dbAuthentication"
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (password: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeFakeUser = (): UserModel => ({
+  id: 'id',
+  name: 'any_name',
+  email: 'email@email.com',
+  password: 'hashed_password'
+})
+
+const makeFakeUserData = (): any => ({
+  email: 'email@email.com',
+  password: 'password'
+})
+
+const makeLoadUserByEmailRepo = (): LoadUserByEmailRepo => {
+  class LoadUserByEmailRepoStub implements LoadUserByEmailRepo {
+    async load (email: string): Promise<UserModel> {
+      const user: UserModel = makeFakeUser()
+      return new Promise(resolve => resolve(user))
+    }
+  }
+  return new LoadUserByEmailRepoStub()
+}
+
+interface SUTTypes {
+  sut: DbAuthentication
+  encrypterStub: Encrypter
+  loadUserByEmailRepoStub: LoadUserByEmailRepo
+}
+
+const makeSUT = (): SUTTypes => {
+  const encrypterStub = makeEncrypter()
+  const loadUserByEmailRepoStub = makeLoadUserByEmailRepo()
+  const sut = new DbAuthentication(loadUserByEmailRepoStub)
+
+  return {
+    sut,
+    encrypterStub,
+    loadUserByEmailRepoStub
+  }
+}
+
+describe('DbLogin UseCase', () => {
+  test('Should DbAuth call LoadUserByEmail with correct email', async () => {
+    const { sut, loadUserByEmailRepoStub } = makeSUT()
+    const getSpy = jest.spyOn(loadUserByEmailRepoStub, 'load')
+
+    await sut.auth(makeFakeUserData()) 
+
+    expect(getSpy).toHaveBeenCalledWith('email@email.com')
+  })
+});
