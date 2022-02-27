184ea9f0db1e97914b48b2ee3af182b3f8193e36
Joismar <joismar@outlook.com>
2022-02-27 14:14:16 -0300

    feat: enrure DbAuth call HashComparer with correct password

--- src/data/useCases/authentication/dbAuthentication.ts
+++ src/data/useCases/authentication/dbAuthentication.ts
@@ -1,11 +1,19 @@
 import { Authentication, AuthModel } from "../../../domain/useCases/authentication"
 import { LoadUserByEmailRepo } from "../../interfaces/loadUserByEmailRepo";
+import { HashComparer } from "../../interfaces/security/hashComparer";
 
 export class DbAuthentication implements Authentication {
-  constructor(private readonly loadUserByEmailRepo: LoadUserByEmailRepo) {}
+  constructor(
+    private readonly loadUserByEmailRepo: LoadUserByEmailRepo,
+    private readonly hashComparer: HashComparer
+  ) {}
 
   async auth(auth: AuthModel): Promise<string> {
-    await this.loadUserByEmailRepo.load(auth.email)
+    const user = await this.loadUserByEmailRepo.load(auth.email)
+    if (user) {
+      await this.hashComparer.compare(auth.password, user.password)
+    }
+
     return null
   }
 }
