8fee285cae71b0e7207951dfe5a431dffff74f21
Joismar <joismar@outlook.com>
2022-02-28 02:55:31 -0300

    refactor: create error middleware for express side errors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser, contentType, cors } from '../middlewares'
+import { error } from '../middlewares/error'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
+  app.use(error)
 }
