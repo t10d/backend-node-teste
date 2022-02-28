706735c53ade9265da14b0faf13c4e35d9cae39b
Joismar <joismar@outlook.com>
2022-02-28 01:45:13 -0300

    feat: verify if email alread exists

--- src/data/useCases/addUser/dbAddUser.ts
+++ src/data/useCases/addUser/dbAddUser.ts
@@ -1,16 +1,22 @@
 import { Hasher } from "../../interfaces/security/hasher"
+import { GetUserByEmailRepo } from "../authentication/interfaces"
 import { UserModel, AddUser, AddUserModel, AddUserRepo } from "./interfaces"
 
 export class DbAddUser implements AddUser {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addUserRepository: AddUserRepo
+    private readonly addUserRepository: AddUserRepo,
+    private readonly getUserByEmailRepo: GetUserByEmailRepo
   ) {}
 
   async add (userData: AddUserModel): Promise<UserModel> {
-    const hashedPassword = await this.hasher.hash(userData.password)
-    const user = await this.addUserRepository.add(
-      Object.assign({}, userData, { password: hashedPassword }))
-    return new Promise(resolve => resolve(user))
+    const findUser = await this.getUserByEmailRepo.getByEmail(userData.email)
+    if (!findUser) {
+      const hashedPassword = await this.hasher.hash(userData.password)
+      const user = await this.addUserRepository.add(
+        Object.assign({}, userData, { password: hashedPassword }))
+      return new Promise(resolve => resolve(user))
+    }
+    return null
   }
 }
