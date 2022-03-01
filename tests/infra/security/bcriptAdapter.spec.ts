e55d75320a407844d437c7be0b59f578bf13959f
Joismar <joismar@outlook.com>
2022-02-28 21:18:15 -0300

    refactor: improve tests visualization

--- tests/infra/security/bcriptAdapter.spec.ts
+++ tests/infra/security/bcriptAdapter.spec.ts
@@ -26,69 +26,73 @@ jest.mock('bcrypt', () => ({
 }))
 
 describe('Bcript Adapter', () => {
-  test('Should call BcriptAdapter.hash with correct value', async () => {
-    const { sut, salt } = makeSUT()
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.hash('any_value')
-
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  describe('hash', () => {
+    test('Should call BcriptAdapter.hash with correct value', async () => {
+      const { sut, salt } = makeSUT()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash('any_value')
+  
+      expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+  
+    test('Should BcriptAdapter.hash return a hash on success', async () => {
+      const { sut } = makeSUT()
+      const hashedValue = await sut.hash('any_value')
+  
+      expect(hashedValue).toBe('hashedValue')
+    }) 
+  
+    test('Should throws if BcriptAdapter.hash throws', async () => {
+      const { sut } = makeSUT()
+  
+      // mock bcrypt with a thrown error
+      jest.spyOn(bcrypt, 'hash')
+        .mockImplementationOnce(
+          () => new Promise((resolve, rejects) => rejects(new Error())))
+  
+      const promiseHashedValue = sut.hash('any_value')
+  
+      await expect(promiseHashedValue).rejects.toThrow()
+    })
   })
+  
+  describe('compare', () => {
+    test('Should call BcriptAdapter.compare with correct value', async () => {
+      const { sut } = makeSUT()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('any_value', 'hash')
 
-  test('Should BcriptAdapter.hash return a hash on success', async () => {
-    const { sut } = makeSUT()
-    const hashedValue = await sut.hash('any_value')
+      expect(compareSpy).toHaveBeenCalledWith('any_value', 'hash')
+    })
 
-    expect(hashedValue).toBe('hashedValue')
-  }) 
+    test('Should return true in BcriptAdapter.compare success', async () => {
+      const { sut } = makeSUT()
+      const isValid = await sut.compare('any_value', 'hash')
 
-  test('Should throws if BcriptAdapter.hash throws', async () => {
-    const { sut } = makeSUT()
+      expect(isValid).toBe(true)
+    }) 
 
-    // mock bcrypt with a thrown error
-    jest.spyOn(bcrypt, 'hash')
-      .mockImplementationOnce(
-        () => new Promise((resolve, rejects) => rejects(new Error())))
+    test('Should return false in BcriptAdapter.compare fail', async () => {
+      const { sut } = makeSUT()
+      jest.spyOn(bcrypt, 'compare')
+        .mockImplementationOnce(() => false)
+      
+      const isValid = await sut.compare('any_value', 'hash')
 
-    const promiseHashedValue = sut.hash('any_value')
+      expect(isValid).toBe(false)
+    }) 
 
-    await expect(promiseHashedValue).rejects.toThrow()
-  })
+    test('Should throws if BcriptAdapter.compare throws', async () => {
+      const { sut } = makeSUT()
 
-  test('Should call BcriptAdapter.compare with correct value', async () => {
-    const { sut } = makeSUT()
-    const compareSpy = jest.spyOn(bcrypt, 'compare')
-    await sut.compare('any_value', 'hash')
+      // mock bcrypt with a thrown error
+      jest.spyOn(bcrypt, 'compare')
+        .mockImplementationOnce(
+          () => new Promise((resolve, rejects) => rejects(new Error())))
 
-    expect(compareSpy).toHaveBeenCalledWith('any_value', 'hash')
-  })
+      const promise = sut.compare('any_value', 'hash')
 
-  test('Should return true in BcriptAdapter.compare success', async () => {
-    const { sut } = makeSUT()
-    const isValid = await sut.compare('any_value', 'hash')
-
-    expect(isValid).toBe(true)
-  }) 
-
-  test('Should return false in BcriptAdapter.compare fail', async () => {
-    const { sut } = makeSUT()
-    jest.spyOn(bcrypt, 'compare')
-      .mockImplementationOnce(() => false)
-    
-    const isValid = await sut.compare('any_value', 'hash')
-
-    expect(isValid).toBe(false)
-  }) 
-
-  test('Should throws if BcriptAdapter.compare throws', async () => {
-    const { sut } = makeSUT()
-
-    // mock bcrypt with a thrown error
-    jest.spyOn(bcrypt, 'compare')
-      .mockImplementationOnce(
-        () => new Promise((resolve, rejects) => rejects(new Error())))
-
-    const promise = sut.compare('any_value', 'hash')
-
-    await expect(promise).rejects.toThrow()
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
--- tests/infra/security/jwtAdapter.spec.ts
+++ tests/infra/security/jwtAdapter.spec.ts
@@ -19,31 +19,33 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('JWT Adapter', () => {
-  test('Should call sign with correct values', async () => {
-    const { sut } = makeSUT()
-
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt('id')
-
-    expect(signSpy).toHaveBeenCalledWith({ id: 'id' }, 'secret')
-  })
-
-  test('Should return a token on sign success', async () => {
-    const { sut } = makeSUT()
-
-    const accessToken = await sut.encrypt('id')
-
-    expect(accessToken).toBe('token')
-  })
-
-  test('Should throw if sign throws', async () => {
-    const { sut } = makeSUT()
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
-      throw new Error()
+  describe('sign', () => {
+    test('Should call sign with correct values', async () => {
+      const { sut } = makeSUT()
+  
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('id')
+  
+      expect(signSpy).toHaveBeenCalledWith({ id: 'id' }, 'secret')
     })
-
-    const promise = sut.encrypt('id')
-
-    expect(promise).rejects.toThrow()
+  
+    test('Should return a token on sign success', async () => {
+      const { sut } = makeSUT()
+  
+      const accessToken = await sut.encrypt('id')
+  
+      expect(accessToken).toBe('token')
+    })
+  
+    test('Should throw if sign throws', async () => {
+      const { sut } = makeSUT()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
+      })
+  
+      const promise = sut.encrypt('id')
+  
+      expect(promise).rejects.toThrow()
+    })
   })
 })
