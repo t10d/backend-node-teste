93cbe9b80d9b642781d67da7d503abb21a0e6981
Joismar <joismar@outlook.com>
2022-03-01 12:24:23 -0300

    refactor: remove fastglobe for fix route import in build

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,13 +1,13 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
 
-  // map routes.ts files and pass a router
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-    (await import(`../../../${file}`)).default(router)
-  })
+  readdirSync(`${__dirname}/../routes/`)
+    .map(async file => {
+      (await import(`${__dirname}/../routes/${file}`)).default(router)
+    })
 }
