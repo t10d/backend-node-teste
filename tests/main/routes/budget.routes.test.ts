b1f354bf0b791781147c72aba20f979d0f99b432
Joismar <joismar@outlook.com>
2022-03-01 00:23:04 -0300

    test: integrate accesstoken to bdget routes test

--- tests/main/routes/budget.routes.test.ts
+++ tests/main/routes/budget.routes.test.ts
@@ -11,83 +11,118 @@ const makeBudget = (): any => ({
   totalProjected: 420
 })
 
-
 const makeAddUser = (): AddUserModel => ({
   name: 'name',
   email: 'email@email.com',
   password: 'hashed_password'
 })
 
-describe('POST /budget', () => {
-  beforeAll(() => {
-    FirestoreHelper.connect()
-  })
-  
-  beforeEach(async () => {
-    await FirestoreHelper.deleteAll('budgets')
+let accessToken = null
+
+describe('Budget Routes', () => {
+  afterAll(async () => {
+    await FirestoreHelper.deleteAll('users')
   })
 
-  test('Should return 403 and an budget on add success without accessToken', async () => {
-    await request(app)
-      .post('/api/budget')
-      .send(makeBudget())
-      .expect(403)
-  })
-
-  test('Should return 200 and an budget on add success', async () => {
-    const userDoc = FirestoreHelper.getCollection('users').doc()
-    const accessToken = sign({ id: userDoc.id }, env.jwtSecret)
-    const userObject = { 
-      id: userDoc.id, 
-      ...makeAddUser(), 
-      role: 'user',
-      accessToken: accessToken
-    }
-    await userDoc.set(userObject)
+  describe('POST /budget', () => {
+    beforeAll(() => {
+      FirestoreHelper.connect()
+    })
     
-    await request(app)
-      .post('/api/budget')
-      .set('x-access-token', accessToken)
-      .send(makeBudget())
-      .expect(200)
-  }) 
+    beforeEach(async () => {
+      await FirestoreHelper.deleteAll('budgets')
+    })
 
-  // test('Should return 400 if missing params or incorrect params', async () => {
-  //   await request(app)
-  //     .post('/api/budget')
-  //     .send({
-  //       name: 'budget_name',
-  //       totalRealized: 42
-  //     })
-  //     .expect(400)
+    describe('without accessToken', () => {
+      test('Should return 403 and an budget on add success without accessToken', async () => {
+        await request(app)
+          .post('/api/budget')
+          .send(makeBudget())
+          .expect(403)
+      })
+    })
 
-  //   await request(app)
-  //     .post('/api/budget')
-  //     .send({
-  //       totalProjected: 420
-  //     })
-  //     .expect(400)
-  // })
+    describe('with accessToken', () => {
+      beforeAll(async () => {
+        const userDoc = FirestoreHelper.getCollection('users').doc()
+        accessToken = sign({ id: userDoc.id }, env.jwtSecret)
+        const userObject = { 
+          id: userDoc.id, 
+          ...makeAddUser(), 
+          role: 'user',
+          accessToken: accessToken
+        }
+        await userDoc.set(userObject)
+      })
+
+      test('Should return 200 and an budget on add success', async () => {
+        await request(app)
+          .post('/api/budget')
+          .set('x-access-token', accessToken)
+          .send(makeBudget())
+          .expect(200)
+      }) 
+
+      test('Should return 400 if missing params or incorrect params', async () => {
+          const fakeBudget = makeBudget()
+
+          for (const key in makeBudget()) {
+            const newFakeBudget = Object.assign({}, fakeBudget)
+            delete newFakeBudget[key]
+      
+            await request(app)
+              .post('/api/budget')
+              .set('x-access-token', accessToken)
+              .send(newFakeBudget)
+              .expect(400)
+          }
+      })
+    })
+  })
+
+  describe('DELETE /budget', () => {
+    beforeAll(() => {
+      FirestoreHelper.connect()
+    })
+    
+    beforeEach(async () => {
+      await FirestoreHelper.deleteAll('budgets')
+    })
+
+    describe('without accessToken', () => {
+      test('Should return 403 and an budget on add success without accessToken', async () => {
+        await request(app)
+          .delete('/api/budget/budget_id')
+          .expect(403)
+      })
+    })
+
+    describe('with accessToken', () => {
+      beforeAll(async () => {
+        const userDoc = FirestoreHelper.getCollection('users').doc()
+        accessToken = sign({ id: userDoc.id }, env.jwtSecret)
+        const userObject = { 
+          id: userDoc.id, 
+          ...makeAddUser(), 
+          role: 'user',
+          accessToken: accessToken
+        }
+        await userDoc.set(userObject)
+      })
+
+      test('Should return 200 and an budget on delete success', async () => {
+        await request(app)
+          .delete('/api/budget/budget_id')
+          .set('x-access-token', accessToken)
+          .expect(200)
+      })
+
+      test('Should return 404 if missing params or incorrect params', async () => {
+        await request(app)
+          .delete('/api/budget')
+          .set('x-access-token', accessToken)
+          .expect(404)
+      })
+    })
+  })
 })
-
-// describe('DELETE /budget', () => {
-//   beforeAll(() => {
-//     FirestoreHelper.connect()
-//   })
-  
-//   beforeEach(async () => {
-//     await FirestoreHelper.deleteAll('budgets')
-//   })
-
-//   test('Should return 200 and an budget on delete success', async () => {
-//     await request(app)
-//       .delete('/api/budget/budget_id')
-//       .expect(200)
-//   })
-
-//   test('Should return 404 if missing params or incorrect params', async () => {
-//     await request(app)
-//       .delete('/api/budget')
-//       .expect(404)
-//   })
-// })
