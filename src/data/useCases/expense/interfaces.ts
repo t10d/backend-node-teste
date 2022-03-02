c17e9ca1ea4cfe587f90dd264dc4585564c82a5d
Joismar <joismar@outlook.com>
2022-03-02 19:04:17 -0300

    feat: create UpdateExpense files for data layer

--- src/data/useCases/expense/interfaces.ts
+++ src/data/useCases/expense/interfaces.ts
@@ -1,3 +1,4 @@
 export * from "../../../domain/models"
 export * from "../../../domain/useCases"
 export * from "../../interfaces/db/expense/addExpenseRepo"
+export * from "../../interfaces/db/expense/updateExpenseRepo"
