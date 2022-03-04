10790a5bfe6c999c23e947610b62b7a778e50723
Joismar <joismar@outlook.com>
2022-03-03 21:45:09 -0300

    tests: create update status routes test

--- tests/main/routes/invite.routes.test.ts
+++ tests/main/routes/invite.routes.test.ts
@@ -119,7 +119,7 @@ describe('Invite Routes', () => {
         await FirestoreHelper.getCollection('invites').doc('invite_id').set(makeInvite(date))
       })
 
-      test('Should return 200 and an invite on add success', async () => {
+      test('Should return 200 and an invite on delete success', async () => {
         await request(app)
           .delete('/api/invite/invite_id')
           .set('x-access-token', accessToken)
@@ -130,6 +130,59 @@ describe('Invite Routes', () => {
       test('Should return 404 if missing id', async () => {
         await request(app)
           .delete('/api/invite')
+          .set('x-access-token', accessToken)
+          .send()
+          .expect(404)
+      })
+    })
+  })
+
+  describe('PATCH /invite_status/:id', () => {
+    beforeAll(() => {
+      FirestoreHelper.connect()
+    })
+    
+    afterAll(async () => {
+      await FirestoreHelper.deleteCollection('invites', 100)
+    })
+
+    describe('without accessToken', () => {
+      test('Should return 403 and an invite status update without accessToken', async () => {
+        await request(app)
+          .patch('/api/invite_status/invite_id')
+          .send()
+          .expect(403)
+      })
+    })
+
+    describe('with accessToken', () => {
+      beforeAll(async () => {
+        await FirestoreHelper.deleteCollection('users', 100)
+        const userDoc = FirestoreHelper.getCollection('users').doc()
+        accessToken = sign({ id: userDoc.id }, env.jwtSecret)
+        const userObject = { 
+          id: userDoc.id, 
+          ...makeAddUser(), 
+          role: 'user',
+          accessToken: accessToken
+        }
+        await userDoc.set(userObject)
+        await FirestoreHelper.getCollection('invites').doc('invite_id').set(makeInvite(date))
+      })
+
+      test('Should return 200 and an invite on status update success', async () => {
+        await request(app)
+          .patch('/api/invite_status/invite_id')
+          .set('x-access-token', accessToken)
+          .send({
+            status: 'any_status'
+          })
+          .expect(200)
+      })
+
+      test('Should return 404 if missing id', async () => {
+        await request(app)
+          .patch('/api/invite_status')
           .set('x-access-token', accessToken)
           .send()
           .expect(404)
