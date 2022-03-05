afd48962c44ea23b93769c00ef75516e931fee22
Joismar <joismar@outlook.com>
2022-03-05 16:20:41 -0300

    fix: invite routes correct appointment

--- src/main/routes/invite.routes.ts
+++ src/main/routes/invite.routes.ts
@@ -4,12 +4,13 @@ import { expressMiddlewareAdapter } from "../adapters/expressMiddlewareAdapter"
 import { makeAddInviteController } from "../factories/invite/makeAddInviteController"
 import { makeDeleteInviteController } from "../factories/invite/makeDeleteInviteController"
 import { makeGetInvitesController } from "../factories/invite/makeGetInvitesController"
+import { makeUpdateInviteStatusController } from "../factories/invite/makeUpdateInviteStatusController"
 import { makeAuthMiddleware } from "../factories/middlewares/makeAuthMiddleware"
 
 export default (router: Router): void => {
   const userAuth = expressMiddlewareAdapter(makeAuthMiddleware('user'))
   router.post('/invite', userAuth, expressAdapter(makeAddInviteController()))
   router.delete('/invite/:id', userAuth, expressAdapter(makeDeleteInviteController()))
-  router.patch('/invite_status/:id', userAuth, expressAdapter(makeDeleteInviteController()))
+  router.patch('/invite_status/:id', userAuth, expressAdapter(makeUpdateInviteStatusController()))
   router.get('/invites', userAuth, expressAdapter(makeGetInvitesController()))
 }
